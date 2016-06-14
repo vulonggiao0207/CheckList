895663c4758a2275299b6ddc0a7bdeea9a2b4958
vulonggiao0207 <vulonggiao0207@yahoo.com>
2016-06-14 23:56:26 +1000

    Design layouts

--- app/src/main/java/com/giao/checklist/MainActivity_Event.java
+++ app/src/main/java/com/giao/checklist/MainActivity_Event.java
@@ -1,7 +1,15 @@
 package com.giao.checklist;
 
+import android.app.Activity;
+
 /**
  * Created by Long on 6/13/2016.
  */
 public class MainActivity_Event {
+    Activity activity;
+    public MainActivity_Event(Activity act)
+    {
+        activity=act;
+    }
+
 }
