806ec5922b69c971378e5db474e5e451de6325ed
vulonggiao0207 <vulonggiao0207@yahoo.com>
2016-06-18 23:58:29 +1000

    create BO

--- app/src/main/java/com/giao/checklist/CheckListBO.java
+++ app/src/main/java/com/giao/checklist/CheckListBO.java
@@ -4,4 +4,11 @@ package com.giao.checklist;
  * Created by Long on 6/16/2016.
  */
 public class CheckListBO {
+    private String checkListName;
+    public CheckListBO(String checklistName)
+    {
+
+    }
+    public String getCheckListName(){return this.checkListName;}
+    public void setCheckListName(String checkListName){this.checkListName=checkListName;}
 }
